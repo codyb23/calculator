3686598ced6bf92ba1521d10694a47868660d855
Cody Banks <codydouglasbanks@gmail.com>
2020-06-30 14:40:54 -0400

    algined buttons to be uniform

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,34 @@ function App() {
       <body>
         <section>
           <form name="calculator">
-            <input class="answerInput" type="text" name="answer" />
+            <input class="answerInput" type="text" name="answer" value="0" />
             <article>
               <input
+                class="acButton"
                 type="button"
-                value=" 1 "
+                value=" AC "
                 onclick="calculator.answer.value += '1'"
               />
               <input
                 type="button"
-                value=" 2 "
+                value=" ÷ "
+                onclick="calculator.answer.value += '2'"
+              />
+            </article>
+            <article>
+              <input
+                type="button"
+                value=" 7 "
+                onclick="calculator.answer.value += '1'"
+              />
+              <input
+                type="button"
+                value=" 8 "
                 onclick="calculator.answer.value += '2'"
               />
               <input
                 type="button"
-                value=" 3 "
+                value=" 9 "
                 onclick="calculator.answer.value += '3'"
               />
               <input
@@ -54,45 +67,41 @@ function App() {
             <article>
               <input
                 type="button"
-                value=" 7 "
+                value=" 1 "
                 onclick="calculator.answer.value += '7'"
               />
               <input
                 type="button"
-                value=" 8 "
+                value=" 2 "
                 onclick="calculator.answer.value += '8'"
               />
               <input
                 type="button"
-                value=" 9 "
+                value=" 3 "
                 onclick="calculator.answer.value += '9'"
               />
               <input
                 type="button"
-                value=" x "
+                value=" + "
                 onclick="calculator.answer.value += '*'"
               />
             </article>
             <article>
               <input
+                class="longButtons"
                 type="button"
-                value=" c "
+                value=" 0 "
                 onclick="calculator.answer.value = ''"
               />
               <input
                 type="button"
-                value=" 0 "
+                value=" . "
                 onclick="calculator.answer.value += '0'"
               />
               <input
                 type="button"
                 value=" = "
                 onclick="calculator.answer.value = eval(calculator.answer.value)"
-              />
-              <input
-                type="button"
-                value=" / "
-                onclick="calculator.answer.value += '/'"
               />
             </article>
           </form>
