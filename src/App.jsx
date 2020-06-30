67b9a23e76b914656ac69b1068ef3942bf763434
Cody Banks <codydouglasbanks@gmail.com>
2020-06-30 16:49:20 -0400

    add on clicks for multi and div

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [chosenNumber, setChosenNumber] = useState(0)
+  const [operation, setOperation] = useState('')
+
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  const buttons = numbers[chosenNumber]
+
+  console.log(buttons)
+  console.log(operation)
+
+  const setNumberFromButton = (event) => setChosenNumber(event)
+  const setOperationType = (type) => setOperation(type)
   return (
     <main>
-      <div class="calculator">
-        <div class="display">0</div>
-        <div class="buttons">
-          <button class="button fn">AC</button>
-          <button class="button fn">&#177;</button>
-          <button class="button fn">&#37;</button>
-          <button class="button op">&#247;</button>
-          <button class="button">7</button>
-          <button class="button">8</button>
-          <button class="button">9</button>
-          <button class="button op">&#215;</button>
-          <button class="button">4</button>
-          <button class="button">5</button>
-          <button class="button">6</button>
-          <button class="button op">&#8722;</button>
-          <button class="button">1</button>
-          <button class="button">2</button>
-          <button class="button">3</button>
-          <button class="button op">&#43;</button>
-          <button class="button x2">0</button>
-          <button class="button">.</button>
-          <button class="button op">&#61;</button>
+      <div className="calculator">
+        <div className="display">
+          {chosenNumber}
+          {operation}
+        </div>
+        <div className="buttons">
+          <button className="button fn">AC</button>
+          <button className="button fn">&#177;</button>
+          <button className="button fn">&#37;</button>
+          <button className="button op" onClick={() => setOperationType('/')}>
+            &#247;
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="7"
+          >
+            7
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="8"
+          >
+            8
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="9"
+          >
+            9
+          </button>
+          <button className="button op" onClick={() => setOperationType('*')}>
+            &#215;
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="4"
+          >
+            4
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="5"
+          >
+            5
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="6"
+          >
+            6
+          </button>
+          <button className="button op">&#8722;</button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="1"
+          >
+            1
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="2"
+          >
+            2
+          </button>
+          <button
+            className="button"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="3"
+          >
+            3
+          </button>
+          <button className="button op">&#43;</button>
+          <button
+            className="button x2"
+            onClick={(event) => setNumberFromButton(event.target.value)}
+            value="0"
+          >
+            0
+          </button>
+          <button className="button">.</button>
+          <button className="button op">&#61;</button>
         </div>
       </div>
     </main>
